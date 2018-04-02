1d24feda3799fd773571799ebba51af803d3a504
USER25 <1>
2018-04-02 17:29:09 +0900

    11123

--- 01_jumptopy/chap05/chap05_practice/q2_ver2.py
+++ 01_jumptopy/chap05/chap05_practice/q2_ver2.py
@@ -3,4 +3,4 @@ sys.path.append("C:\\py_workspace\\openbigdata\\01_jumptopy\\chap05\\my_modules"
 from calculator2 import Calculator
 
 cal1=Calculator([1,2,3,4,5])
-print(cal1.sum())
+print(cal1.sum())123
